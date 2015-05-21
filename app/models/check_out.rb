2a6c378314e0560d75c41b45a566be27d6818946
Cesar Juliano <pochettejuliano@gmail.com>
2015-05-21 14:01:51 -0400

    Changing configuration values

--- app/models/check_out.rb
+++ app/models/check_out.rb
@@ -4,7 +4,7 @@ class CheckOut < ActiveRecord::Base
   belongs_to :user
   has_one :fine
 
-  CHECK_OUT_PERIOD = 21
-  RENEWAL_COUNT = 3
+  CHECK_OUT_PERIOD = 7
+  RENEWAL_COUNT = 1
 
 end
--- app/models/fine.rb
+++ app/models/fine.rb
@@ -1,6 +1,6 @@
 class Fine < ActiveRecord::Base
 
-  DAILY_FINE_AMOUNT = 20
+  DAILY_FINE_AMOUNT = 50
   DAYS_IN_SECONDS = 86400
 
 end
