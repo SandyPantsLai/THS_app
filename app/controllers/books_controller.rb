2a49dcf6c6ede4ab7b98889cff717763c6c4f625
Harrison Taylor <h.taylor@project89.org>
2015-05-22 14:24:45 -0400

    Search functionality working nicely

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -35,9 +35,8 @@ class BooksController < ApplicationController
   def index
     @books = []
     if params[:search]
-
       params[:search].split.each do |s|
-        @books << Book.where("LOWER(title) LIKE LOWER(?) OR LOWER(first_name) LIKE LOWER(?) OR LOWER(last_name) LIKE LOWER(?) OR LOWER(subject) LIKE LOWER(?)", "%#{s}%", "%#{s}%", "%#{s}%", "%#{s}%")
+        @books << Book.where("LOWER(title) LIKE LOWER(?) OR LOWER(first_name) LIKE LOWER(?) OR LOWER(last_name) LIKE LOWER(?)", "%#{s}%", "%#{s}%", "%#{s}%")
       end
       @books.flatten!
       @books.uniq!
