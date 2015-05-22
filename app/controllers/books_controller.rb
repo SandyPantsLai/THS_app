7d91a77609f971aeda9f73f0a51b2167fc71a7d1
Harrison Taylor <h.taylor@project89.org>
2015-05-22 14:49:54 -0400

    Search works perfectly now

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,19 +33,22 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = []
-    if params[:search]
-      params[:search].split.each do |s|
-        @books << Book.where("LOWER(title) LIKE LOWER(?) OR LOWER(first_name) LIKE LOWER(?) OR LOWER(last_name) LIKE LOWER(?)", "%#{s}%", "%#{s}%", "%#{s}%")
-      end
-      @books.flatten!
-      @books.uniq!
-    else
-      @books = Book.all
-    end
-    if request.xhr?
-      render :partial => "book", :collection => @books
-    end
+    @search = Book.search(params[:q])
+    @books = @search.result
+
+    # @books = []
+    # if params[:search]
+    #   params[:search].split.each do |s|
+    #     @books << Book.where("LOWER(title) LIKE LOWER(?) OR LOWER(first_name) LIKE LOWER(?) OR LOWER(last_name) LIKE LOWER(?)", "%#{s}%", "%#{s}%", "%#{s}%")
+    #   end
+    #   @books.flatten!
+    #   @books.uniq!
+    # else
+    #   @books = Book.all
+    # end
+    # if request.xhr?
+    #   render :partial => "book", :collection => @books
+    # end
   end
 
   def delete
