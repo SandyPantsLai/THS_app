a34b82e39ad67af1e77996739a6c1ae06d57a8fc
Harrison Taylor <h.taylor@project89.org>
2015-05-25 14:56:33 -0400

    Added some test info, changed seed file, added pagination.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+def setup
+    user = users(:one)
+    login_user(user)
+end
   # test "the truth" do
   #   assert true
   # end
