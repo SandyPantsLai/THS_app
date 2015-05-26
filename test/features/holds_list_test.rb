4a0d3455ac742dbec895db8d10de1153a0bb63a6
Sandy Lai <sandywslai@gmail.com>
2015-05-26 01:01:40 -0400

    added test to ensure admin sees all user holds on holds index page

--- test/features/holds_list_test.rb
+++ test/features/holds_list_test.rb
@@ -7,4 +7,21 @@ feature "HoldsList" do
     visit holds_path
     page.has_content?(Book.find(@hold.book_id).title)
   end
+
+  scenario "holds index page displays a list of ALL holds if the user is an admin" do
+    @user1 = create(:admin)
+    @user2 = create(:user)
+    @user3 = create(:user)
+    @hold1 = create(:hold, user: @user1)
+    @hold2 = create(:hold, user: @user2)
+    @hold3 = create(:hold, user: @user3)
+    @hold4 = create(:hold, user: @user3)
+    @holds = [@hold1, @hold2, @hold3, @hold4]
+
+    visit holds_path
+    @holds.each do |hold|
+      page.has_selector?("user")
+      page.has_content?(Book.find(hold.book_id).title)
+    end
+  end
 end
