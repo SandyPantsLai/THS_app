8b60ac1dddfaae3913589096b7eda1d4de13d039
Sandy Lai <sandywslai@gmail.com>
2015-05-27 12:45:09 -0400

    added test to see if user can check out a book by entering URL directly.  Currently failing, will speak with Cesar about fixing controller to pass

--- test/features/check_out_book_test.rb
+++ test/features/check_out_book_test.rb
@@ -2,25 +2,31 @@ require "test_helper"
 
 feature "CheckOutBook" do
   setup do
-    @admin = create(:admin)
-    @user1 = create(:user)
-    @user2 = create(:user)
     @book = create(:book)
-    @copy1 = create(:book_copy, book: @book)
-    @copy2 = create(:book_copy, book: @book)
-    @check_out = create(:check_out)
+    @copy = create(:book_copy, book: @book)
     visit login_path
+  end
+
+  scenario "An admin can check out a book" do
+    @admin = create(:admin)
+    @user = create(:user)
+
     fill_in "Email", with: @admin.email
     fill_in "Password", with: "1234"
     click_button "Login"
     click_link "Admin Panel"
     click_link "Checkouts"
+    fill_in "User Id", with: @user.id
+    fill_in "Book Copy Id", with: @copy.id
+    click_button "Check Out Book"
+    assert_equal(CheckOut.last.book_copy, @copy)
   end
 
-  scenario "An admin can check out a book" do
-    fill_in "User Id", with: @user1.id
-    fill_in "Book Copy Id", with: @copy1.id
-    click_button "Check Out Book"
-    assert_equal(CheckOut.last.book_copy, @copy1)
+  scenario "A normal user cannot check out a book" do
+    @user = create(:user)
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: "4321"
+    visit new_check_out_path
+    page.has_content?("error")
   end
 end
