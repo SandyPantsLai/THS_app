9176a74312bb489d1f40415ae28662352fc4e44a
Sandy Lai <sandywslai@gmail.com>
2015-05-27 11:47:55 -0400

    Added test to update next hold without pickup date with a pickup date if hold with pickup date is cancelled

--- test/features/cancel_holds_test.rb
+++ test/features/cancel_holds_test.rb
@@ -7,14 +7,19 @@ feature "CancelHolds" do
     @hold1 = create(:hold, user: @user1, pickup_expiry: Time.now + 2.days)
     @hold2 = create(:hold, book: @hold1.book, user: @user2)
     visit login_path
-  end
-
-  scenario "A user can cancel their own hold" do
     fill_in "Email", with: @user1.email
     fill_in "Password", with: "4321"
     click_button "Login"
     visit holds_path
+  end
+
+  scenario "When a hold is cancelled, it is removed from the list of holds" do
     page.find('.cancel').click
     assert_equal(Hold.first, @hold2)
+  end
+
+  scenario "If a hold with a pickup_expiry is cancelled, the next hold in queue is updated with a pickup_expiry" do
+    page.find('.cancel').click
+    assert @hold2.pickup_expiry
   end
 end
