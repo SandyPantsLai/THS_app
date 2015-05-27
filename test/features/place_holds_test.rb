70ca4126806b19c269cd810e207703ea4a535870
Sandy Lai <sandywslai@gmail.com>
2015-05-27 10:34:24 -0400

    Added test to see if there is a copy of a book available, the newly placed hold is available for immediate pick up.

--- test/features/place_holds_test.rb
+++ test/features/place_holds_test.rb
@@ -6,6 +6,9 @@ feature "PlaceHolds" do
     @user2 = create(:user)
     @hold1 = create(:hold, user: @user1)
     @hold2 = create(:hold, user: @user2)
+    @copy1 = create(:book_copy, book: @hold2.book)
+    @copy2 = create(:book_copy, book: @hold2.book)
+    @hold2.pickup_expiry = Time.now + 2.days
     @book = create(:book)
     visit login_path
     page.has_content?("user")
@@ -14,18 +17,23 @@ feature "PlaceHolds" do
     click_button "Login"
   end
 
-  scenario "a user can place a hold on a book if they do not already have an active hold for it" do
-    visit book_path(@book)
-    click_link "Place a hold"
-    page.has_no_content?("We were unable to confirm your hold due to these errors")
-  end
-
   scenario "a user CANNOT place a hold on a book if they already have an active hold for it" do
     visit book_path(@hold1.book)
     page.has_no_link?("Place a hold")
   end
 
+  scenario "a user can place a hold on a book if they do not already have an active hold for it" do
+    visit book_path(@book)
+    click_link "Place a hold"
+    click_button "Confirm Hold"
+    assert_not_equal(Hold.last, @hold2)
+  end
+
   scenario "if there is a copy of a book available, the newly placed hold is available for immediate pick up." do
+    visit book_path(@hold2.book)
+    click_link "Place a hold"
+    click_button "Confirm Hold"
+    page.has_content?("You have until #{Hold.last.pickup_expiry.strftime("%A, %b %e, %Y")} to pick up your book.")
   end
 
 end
