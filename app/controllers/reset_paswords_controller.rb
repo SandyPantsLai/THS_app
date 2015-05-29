98f6793dee703459b6e4c6f17e8a7b0a7ad93285
lucasjohnson <contact@lucasjohnson.co.nz>
2015-05-29 11:22:31 -0400

    Edit method added

--- app/controllers/reset_paswords_controller.rb
+++ app/controllers/reset_paswords_controller.rb
@@ -10,4 +10,12 @@ class ResetPaswordsController < ApplicationController
   	flash[:success] = 'Password reset instructions have been sent to your email.'
   	redirect_to log_in_path
 	end
+
+	def edit
+		@token = params[:id]
+		@user = User.load_from_reset_password_token(@token)
+
+		not_authenticated if @user.blank?
+	end
+	
 end
