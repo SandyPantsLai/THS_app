68d0369b8ee0515ccb4299958dcfbe6aa204fbd5
lucasjohnson <contact@lucasjohnson.co.nz>
2015-05-29 15:44:36 -0400

    Login now rediects to book catalogue

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,10 +6,12 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    if @user = login(params[:email], params[:password], params[:remember_me])
-      format.html {redirect_to books_path(:notice => "Login Successful")}
-    else
-      format.html {flash.now[:alert] = "E-mail and/or password is incorrect"; render :action => 'new'}
+    respond_to do |format|
+      if @user = login(params[:email], params[:password], params[:remember_me])
+        format.html {redirect_to books_path(:notice => "Login Successful")}
+      else
+        format.html {flash.now[:alert] = "E-mail and/or password is incorrect"; render :action => 'new'}
+      end
     end
   end
 
