7388db781b1f0ee11ba70bd70d24d2adde5a3f84
Sandy Lai <sandywslai@gmail.com>
2015-05-29 07:54:46 -0400

    adjusted transactions list test to account for deposits

--- test/features/transactions_list_test.rb
+++ test/features/transactions_list_test.rb
@@ -8,8 +8,10 @@ feature "TransactionsList" do
     @check_out2 = create(:check_out, user: @user)
     @fine1 = create(:fine, amount: 25, check_out_id: @check_out1, settlement_date: Time.now - 2.days)
     @fine2 = create(:fine, amount: 50, check_out_id: @check_out2)
-    @member_fee1 = create(:member_fee, amount: 500, user: @admin, settlement_date: Time.now - 28.days)
+    @member_fee1 = create(:member_fee, amount: 10000, user: @admin, settlement_date: Time.now - 14.days)
     @member_fee2 = create(:member_fee, amount: 1000, user: @user, settlement_date: Time.now - 28.days)
+    @deposit1 = create(:deposit, amount: 4000, user: @admin)
+    @deposit2 = create(:deposit, amount: 4000, user: @user)
     visit login_path
     fill_in "Email", with: @user.email
     fill_in "Password", with: "4321"
@@ -21,8 +23,10 @@ feature "TransactionsList" do
   scenario "As a user, I can see a list of all transaction items with a link to pay what is due" do
     assert page.has_content?(@fine2.amount)
     assert page.has_content?(@member_fee2.amount)
+    assert page.has_content?(@deposit2.amount)
     assert page.has_no_content?(@fine1.amount)
-    assert page.has_no_content?(@member_fee2.amount)
+    assert page.has_no_content?(@member_fee1.amount)
+    assert page.has_no_content?(@deposit1.amount)
     assert page.has_link?(new_charge_path)
   end
 end
