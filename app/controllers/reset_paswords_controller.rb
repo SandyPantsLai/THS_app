f40fd03bb738a859da082bd19c71b7a6f2fa4b79
lucasjohnson <contact@lucasjohnson.co.nz>
2015-05-29 11:13:26 -0400

    Reset password form created, and method set in reset password controller

--- app/controllers/reset_paswords_controller.rb
+++ app/controllers/reset_paswords_controller.rb
@@ -2,6 +2,12 @@ class ResetPaswordsController < ApplicationController
 	skip_before_filter :require_login
 
 	def new
+	end
 
+	def create
+  	@user = User.find_by_email(params[:email])
+  	@user.deliver_reset_password_instructions! if @user
+  	flash[:success] = 'Password reset instructions have been sent to your email.'
+  	redirect_to log_in_path
 	end
 end
