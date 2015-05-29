817e4f8746389e7c3d02851e2fde42f0fef1348b
lucasjohnson <contact@lucasjohnson.co.nz>
2015-05-29 00:00:02 -0400

    Views and controllers deleted that I created in error while chasing the rabbit

--- app/controllers/google-books.rb
+++
@@ -1,7 +0,0 @@
-response = HTTpary.get('https://www.googleapis.com/books/v1/volumes?q=blood+meridian')
-
-puts response.body, response.code, response.message, response.headers.inspect
-
-	response.each do |item|
-		puts item['title']['author']['publisher']['subjects']['isbn']
-	end
