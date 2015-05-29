0b2a8155f017070974d4e431047d51d1a2d57929
lucasjohnson <contact@lucasjohnson.co.nz>
2015-05-29 11:26:05 -0400

    Update method created for password reset

--- app/controllers/reset_paswords_controller.rb
+++ app/controllers/reset_paswords_controller.rb
@@ -17,5 +17,20 @@ class ResetPaswordsController < ApplicationController
 
 		not_authenticated if @user.blank?
 	end
-	
+
+	def update
+	  @token = params[:id]
+	  @user = User.load_from_reset_password_token(@token)
+	 
+	  not_authenticated && return if @user.blank?
+	 
+	  @user.password_confirmation = params[:user][:password_confirmation]
+	  if @user.change_password!(params[:user][:password])
+	    flash[:success] = 'Password was successfully updated.'
+	    redirect_to log_in_path
+	  else
+	    render "edit"
+	  end
+	end
+
 end
